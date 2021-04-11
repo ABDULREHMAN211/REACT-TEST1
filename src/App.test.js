02f3c8ddfcf6388b10bc2944c316e5de7b348204
ABDUL REHMAN <bajwaarehmanbajwa@gmail.com>
2021-04-11 14:47:18 +0500

    react test

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/HELLO WORLD./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('text test', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/HELLO PIAIC./i);
+  expect(linkElement).toBeInTheDocument();
+});
